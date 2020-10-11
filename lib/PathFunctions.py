d803eb651dc2302d43fb4045f1cf66fc09ff6895
machinexa <machinexa@gmail.com>
2020-10-11 21:21:25 +0545

    09:21:25 PM 10/11/2020

--- lib/PathFunctions.py
+++ lib/PathFunctions.py
@@ -1,6 +1,20 @@
 class PathFunction:
     def __init__(self):
         pass
+
+    def merge(self, xpath: str, ypath: str) -> str:
+        if not xpath or not ypath:
+            return ""
+        xpath = self.urlerslasher(xpath) 
+        ypath = self.payloader(ypath)
+        zpath = xpath + ypath
+        return zpath
+    
+    def urlerslasher(self, xpath: str) -> str:
+        if not xpath:
+            return xpath
+        ypath = self.slasher(self.urler(xpath))
+        return ypath
     
     def slasher(self, xpath: str) -> str:
         if not xpath:
@@ -12,6 +26,8 @@ class PathFunction:
         return ypath
     
     def unslasher(self, xpath: str) -> str:
+        if not xpath:
+            return xpath
         if xpath[-1] == '/':
             ypath = xpath[:-1]
         else:
@@ -37,8 +53,19 @@ class PathFunction:
         return ypath
 
     def urler(self, xpath: str) -> str:
+        if not xpath:
+            return xpath
         if not xpath.startswith('http://') and not xpath.startswith('https://'):
             ypath = "http://" + xpath
         else:
             ypath = xpath
+        return ypath
+
+    def unurler(self, xpath: str) -> str:
+        if not xpath:
+            return xpath
+        if not '://' in xpath:
+            ypath = xpath
+        else:
+            ypath = xpath.split('://')[-1]
         return ypath
